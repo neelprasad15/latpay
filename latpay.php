d6e7d2a05c0025fe743d09b84c3972667a5f694f
neelprasad15@gmail.com <neelprasad15@gmail.com>
2019-10-03 17:29:58 +0530

    new lisence

--- latpay.php
+++ latpay.php
@@ -216,10 +216,10 @@ class Latpay extends PaymentModule
     protected function payInput()
     {
         $cart = $this->context->cart;
+        $purchase_summary = Configuration::get('LATPAY_HPS_ACCOUNT_PURCHAS_SUMMARY'); 
         $merchant_id = Configuration::get('LATPAY_HPS_ACCOUNT_MERCHANT_ID');
         $merchant_pw = Configuration::get('LATPAY_HPS_ACCOUNT_MERCHANT_PASSWORD');
         $Secret_Key = Configuration::get('LATPAY_HPS_ACCOUNT_SECRET_KEY');
-        $purchase_summary = Configuration::get('LATPAY_HPS_ACCOUNT_PURCHAS_SUMMARY'); 
         $customer = new Customer($cart->id_customer);
         $address = new Address($cart->id_address_invoice);
         $state = new State($address->id_state);
